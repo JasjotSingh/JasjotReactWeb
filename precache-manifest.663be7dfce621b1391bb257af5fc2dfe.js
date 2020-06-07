6fd470899fa98eeef9bf25d589394e70843d9f04
Jasjot <jasjot_jrarjn1@yahoo.in>
2020-06-07 16:55:15 -0700

    Updates

---
+++ precache-manifest.663be7dfce621b1391bb257af5fc2dfe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63443fc48e9ea0a4a3584668d16b823d",
+    "url": "/JasjotReactWeb/index.html"
+  },
+  {
+    "revision": "d34983cbf57f23019be2",
+    "url": "/JasjotReactWeb/static/js/2.58ec1a59.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/JasjotReactWeb/static/js/2.58ec1a59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb9d71f9c075fb31d4b1",
+    "url": "/JasjotReactWeb/static/js/main.7b30c786.chunk.js"
+  },
+  {
+    "revision": "461d680f18a2ab97447c",
+    "url": "/JasjotReactWeb/static/js/runtime-main.ffeb8314.js"
+  }
+]);
